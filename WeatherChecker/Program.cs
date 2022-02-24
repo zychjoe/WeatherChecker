938cc28a6f0a47073cb5e902419c6a4593e1b25a
Joe Zedd <zychjoe@gmail.com>
2022-02-23 20:05:05 -0500

    initial commit

---
+++ WeatherChecker/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace WeatherChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            Console.WriteLine("Please give me a city name:");
+            var cityName = Console.ReadLine();
+
+            var weatherUrl = $"http://api.openweathermap.org/data/2.5/weather?q={cityName}&appid=e6b1ea90b19a1576a1ea6a730d6cb553&units=imperial";
+
+            var response = client.GetStringAsync(weatherUrl).Result;
+            var formattedResponse = JObject.Parse(response).GetValue("main").ToString();
+
+            Console.WriteLine(formattedResponse);
+            
+
+        }
+    }
+}
